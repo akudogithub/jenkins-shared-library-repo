1b28c92dc00e22d1e913daacc08becc5f36aa88d
akudo <nonsoak@gmail.com>
2022-06-13 13:27:15 +0100

    groovyfile2 modified

--- vars/libraryApp2.groovy
+++ vars/libraryApp2.groovy
@@ -1,23 +1,3 @@
 def call(String repoUrl){
-pipeline {
-       agent any
-        stages {
-           stage("system-resource-check") {
-               steps {
-                   sh 'lscpu'
-                   sh 'java -version'
-               }
-           }
-           stage("Checkout Code") {
-               steps {
-                   git branch: 'main',
-                          url: "${repoUrl}"
-               }
-           }
-           stage("to-check-storage") {
-               steps {
-                   sh 'lsblk'
-               }
-           }
-       }
-}
+  echo "git clone $repoUrl"
+ }
